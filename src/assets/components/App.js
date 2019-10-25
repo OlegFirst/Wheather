629576ad81aa5cd66f22641674652b4ffdd82f95
Oleg <OlegFIRST@ukr.net>
2019-10-25 13:34:26 +0300

    Create main layout

--- src/assets/components/App.js
+++ src/assets/components/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import 'reset-css';
 import './../scss/index.scss';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<Router>
+				<div className="application">
+					<header className="application__header">
+						<h1>Wheather APP</h1>
+					</header>
+					<nav className="application__nav nav">
+						Nav
+					</nav>
+					<section className="application__container container">
+						Section
+					</section>
+				</div>
+			</Router>
+		)
+	}
 }
 
 export default App;
